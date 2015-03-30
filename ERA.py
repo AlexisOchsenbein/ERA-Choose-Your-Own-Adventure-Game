4d4c6a1049638a21806bb89998b09834ce301b1d
unknown <hetzr300@CE105WD554852S.hillnet.ad.hsd.k12.or.us>
2015-03-30 09:50:35 -0700

    I added stuff

--- ERA.py
+++ ERA.py
@@ -13,39 +13,57 @@ w.pack()
 
 def intro():
     """Start of the story"""
-    messagebox.showinfo("ERA", "\n START STORY HERE, INTRO ")
-    choice = simpledialog.askinteger("THE 3 CHOICES FOR GAME")
+    messagebox.showinfo ("Old Lady",
+                         " You're walking down the street and you see a sweet "+\
+                         "old lady trip and fall on her face.")
+    oldlady = simpledialog.askinteger (" Please type the number corresponding to your desired class.",
+                                    " To help the old lady up, press 1 " +\
+                                    " To mind your own buissness and walk past, press 2. " +\
+                                    " To steal her purse while she's down, press 3.")
 
-    if choice == 1:
+
+    if oldlady == 1:
         choice1()
-    elif choice2 == 2:
+    elif oldlady == 2:
         choice2()
-    elif choice3 == 3:
+    elif oldlady == 3:
         choice3()
     else:
         intro()
 
 ############################### Rachel's Function ###############################
 def choice1():
-    choice = simpledialog.askinteger("Choose wisely",
-                                     "This is the next part of the story.  Now you must choose 1 or 2 again.")
+    choice = messagebox.showinfo ("You steal her purse",
+                             " You run by and snatch her purse as she struggles to get up."+\
+                             " You hear her cursing at you as you run off into the near by forest."+\
+                             " You feel you aren't bein chased, and begin rooting through her purse."+\
+                             " Inside you find nothing but a bunch of old lady stuff, until a ragged old"+\
+                             " paper catches your eye. You unfold it and see that it is a treasure map!"+\
+                             " The treasure seems to be somewhere in the forest you're in now, so you set"+\
+                             " off to find it.")
+   
+
+    messagebox.showinfo ("The Forest",
+                             " As you're reading the map, you here voices behind you."+\
+                             " 'That way! Into the forest!' you here and old lady shout."+\
+                             " 'Okay mam we will get your purse back for you.' a deeper, younger"+\
+                             " voice says. You get scared and run off further into the forest. Your"+\
+                             " bad karma catches up to you, as you find yurself tripping into a river"+\
+                             " you forgot was there. You make it to the other side, but part of the map"+\
+                             " is ruined.")
+    messagebox.showinfo(" Lost",
+                            " You walk for a couple hours, trying to remember all of the map"+\
+                            " you can, and have been successful so far, until now. You don't"+\
+                            " remember if you should go left or right.")
+
+    leftorright= simpledialog.askinteger ("Left or Right?",
+                             " Press 1 for right, or 2 for left.")
+
+    if leftorright ==1:
+         Rchoice1 
+    elif leftorright ==2:
+         Rchoice2
     
-    if (choice == 1):
-        messagebox.showinfo("The End",
-                            "You chose right1.  THE END")
-
-    elif (choice == 2):
-        messagebox.showinfo("The End",
-                            "You chose ok2.  THE END")
-    elif (choice == 3):
-        messagebox.showinfo("The End",
-                            "You chose ok3.  THE END")
-        
-    else:
-        choice1()
-
-
-
 
 
 
