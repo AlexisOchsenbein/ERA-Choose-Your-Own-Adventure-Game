f411e71f43495838d8e73b396f9d9c61a6523d61
Alexis Ochsenbein <ochsa642@CE105WD554917S.hillnet.ad.hsd.k12.or.us>
2015-04-22 10:06:59 -0700

    finished 2 branch

--- ERA.py
+++ ERA.py
@@ -6,6 +6,7 @@
 from tkinter import *
 import tkinter.simpledialog
 import tkinter.messagebox
+import random
 
 root = Tk()
 w = Label(root, text="ERA")
@@ -126,6 +127,8 @@ def ignoreOldLady():
     else:
         ignoreOldLady()
 
+        
+
     if getting_ready == 1:
         Prepaired()
         
@@ -154,12 +157,41 @@ def Prepaired():
     purchase = simpledialog.askinteger("Purchase",
                             "Sword (1) cost 5 gold, Chestplate(2) cost 5 gold, Both(3) cost 10 gold,"+\
                             "You decide to purchase...")
+
+
+#Sword"
     if purchase == 1:
         gold = gold - 5
 
         messagebox.showinfo("This Will Do.",
                             "Now that you've got your sword you set of in the direction" +\
                             " that the map specifies.")
+        
+        messagebox.showinfo("Bear,",
+                            "As you walk through the forest following the map you run into an angry"+\
+                            " bear. She dosent look like she is going to let you go without a fight."+\
+                            " Normaly you would run away from this situation but since you bought this sword"+\
+                            " you feel pretty confident and decide to fight the bear.")
+
+        
+        messagebox.showinfo("Die.",
+                            "As you swing the sword at the bear you manage to get a few good cuts in."+\
+                            " After you balance yourself out again you look up at the bear to see the damage"+\
+                            " you did. Looking at the bear you see it hasnt moved since you got there and is not"+\
+                            " amussed with your attempt at defeating it.The  bear stands up and"+\
+                            " takes you out with one big swipe of its claw and rips open your chest."+\
+                            " Your body is left on the forest floor to be eaten by the bear cubs and any other"+\
+                            " critter that walks by your lifeless body. Well at least you tried your best,"+\
+                            " and hey now someone gets a free sword."+\
+                            " THE END.")
+
+        
+
+
+
+
+
+#Chestplate#      
     elif purchase == 2:
         gold = gold - 5
 
@@ -167,19 +199,48 @@ def Prepaired():
                             "Now that you can almost gurantee you most likely won't"+\
                             " get stabed through the chest you feel safe enough to set"+\
                             " of into the direction the map specifies.")
+        
+        messagebox.showinfo("Bear,",
+                            "As you walk through the forest following the map you run into an angry"+\
+                            " bear. She dosent look like she is going to let you go without a fight" +\
+                            " Since you didnt buy a weapon to fight with you dont really know how your"+\
+                            " going to get out of this besides running away. So you turn around as quickly"+\
+                            " as you can and the bear swipes at your back making a loud clashing noise. She hit"+\
+                            " the chestplate! As you continue running along deeper into the forest you notice the"+\
+                            " bear is gone and you dropped the map ahwile ago while fleeing. As you come to a stop and"+\
+                            " realize you're completly lost and have no idea where you are, you sit down next to the closest tree."+\
+                            " You are completly exausted and start to fall asleep, but as you do you here a low growl."+\
+                            " To tierd to get up you pass out and are ripped apart in your sleep by the bear."+\
+                            " THE END.")
+
+#Sword and Chestplate#         
     elif purchase == 3:
         gold = gold - 10
 
         messagebox.showinfo("Let's Go!",
                             "Now that you're looking good but broke you set off in the"+\
                             "direction the map specifies with your sword and chestplate.")
+
+        messagebox.showinfo("Bear",
+                            "As you walk through the forest following your map you run into"+\
+                            " an angry looking bear. The bear dosent look like its going to let you"+\
+                            " go without a fight. Not fearing the callange you pull out your sword and"+\
+                            " aim for the bears heart.")
+        messagebox.showinfo("Kill",
+                            "You  swing the sword twords the bears heart and hit right on."+\
+                            " The bear falls over and lets out a loud roar, knowing that it isnt going"+\
+                            " to get back up you continue on your journy, paying close attention to the map.")
+        Hero()
+
+        
+        
+
+
+
+
+        
     else:
         Prepaired()
-
-        messagebox.showinfo("Bear,",
-                            "As you walk through the forest following the map you run into an angry"+\
-                            " bear")
-    
 
 
 def Unprepaired():
@@ -205,9 +266,82 @@ def Unprepaired():
                         " THE END.")
 
                             ### Why errror when ending def Unprepaired? ###
-root.destroy()
+
     
 
+def Hero():
+    """ The Choice Where you live """
+    
+    messagebox.showinfo("Hero",
+                        "You follow the map for a long while and end up at a large"+\
+                        " cave. You attempt to peer inside but you are only meet with"+\
+                        " darkness. ")
+    
+    messagebox.showinfo("Hero",
+                        "You decide you have to go into the cave if you have already traveled"+\
+                        " this far. As you walk in you hear a strange noise...")
+    messagebox.showinfo("Dragon",
+                        "As your eyes ajust to the dim light coming from deeper within the cave,"+\
+                        " you are frozen in place by the sight of a dragon... Sitting ontop of all"+\
+                        " this gold! Now you have to decide. Try to fight the dragon for the gold(1) or"+\
+                        " sneak past it and try to collect as much as you can before he wakes up and you"+\
+                        " escape(2).")
+    sneak_or_steal = simpledialog.askinteger("Fight OR Flight",
+                                             "1 for fighting the dragon or 2 for sneaking."+\
+                                             " you decide to...")
+    if sneak_or_steal == 1:
+        messagebox.showinfo("Fight",
+                            "You decide to walk strait up to the dragon and announce your"+\
+                            " here. He slowly starts to get up looking strait at you. When the"+\
+                            " dragon is almost fully standing you charge at it with your sword."+\
+                            " The dragon suffers a massive cut right to the belly but continues to fight you.")
+        dragon_Fight()
+        
+        
+
+    elif sneak_or_steal == 2:
+        messagebox.showinfo("Sneak",
+                            "As you try your best to sneak around the dragon,"+\
+                            " your sword scratchs the ground behind the beast and it"+\
+                            " wake up. You try to find a place to hide but its to late."+\
+                            " the dragon turns around and blasts you with fire. You are"+\
+                            " now super crispy (and also dead) and the dragon returns to rest."+\
+                            " THE END.")
+
+def dragon_Fight():
+    """ Fighting the dragon """
+    
+    dragon_hp = random.randint (50,150)
+    dragon_dmg = random.randint (5,50)
+    hero_hp = random.randint (100,200)
+    hero_dmg = random.randint (25,50)
+
+    while hero_hp >= 0 or dragon_hp >=0:
+        attack = simpledialog.askinteger("Attack.",
+                                         " Press 1 to attack dragon.")
+
+
+        if attack == 1:
+            dragon_hp -= hero_dmg
+            hero_hp -= dragon_dmg
+            messagebox.showinfo("Dragon Fight",
+                                " You took {} damage.".format(dragon_dmg)+\
+                                " Your health is now {}.".format(hero_hp)+\
+                                " The dragon has {}.".format(dragon_hp))
+
+        if hero_hp <= 0 or dragon_hp <= 0:
+            break
+        if hero_hp <= 0:
+            messagebox.showinfo("Died",
+                                "As hard as you tried the dragon still manages to kill you."+\
+                                " You have died. THE END.")
+        elif hero_hp >=1:
+                messagebox.showinfo("Won",
+                                    "You kill the dragon and all the treasure is now yours."+\
+                                    " The End.")
+
+                
+        
     
     
     
